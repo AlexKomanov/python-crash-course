f1e08ad3cb8bf1eb64e99cd44da2d2d582520a08
AlexKomanov <akomanov88@gmail.com>
2024-01-14 23:35:30 +0200

    Adding 03_strings_integers_floats.py

---
+++ 03_strings_integers_floats.py
@@ -0,0 +1,22 @@
+#  strings  -> "Text" 'Text' "Text with 12345"
+
+print("Hello")
+print('Hello')
+print("123 + 1")
+print(type("123"))
+print(type("""Text"""))
+
+###########
+
+#  Integers  -> 1 10000000 -23424234324234
+print(123)
+print(1000000 + 1)
+print(-1000000 + 1)
+print(type(1000000))
+
+###########
+#  Floats  -> 1.256 1000000.36 -1000000.365
+print(1233.65)
+print(1000000.6987 + 1)
+print(-1000000.4894 + 1)
+print(type(1000000.25974))
