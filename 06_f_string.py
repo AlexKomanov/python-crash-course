8cc1a5c5d50e81cf6d04f72947b083bd967d58bd
AlexKomanov <akomanov88@gmail.com>
2024-01-15 09:18:02 +0200

    Adding 06_f_string.py

---
+++ 06_f_string.py
@@ -0,0 +1,12 @@
+name = "Alex"
+greeting = "Hello"
+age = 35
+print(greeting + " " + name)
+print(f"{greeting} {name}")
+# print(f'{greeting} {name}')
+# print(F'{greeting} {name}')
+
+
+print("My name is " + name + ". My age is " + str(age))
+print(f"My name is {name}. My age is {age}")
+print(f"My name is {name.upper()}. Next year - my age is going to be {age + 1}")
