1ff4a193366d395769f39dc7b6840f23513abced
AlexKomanov <akomanov88@gmail.com>
2024-01-15 08:08:53 +0200

    Adding 05_variables.py

---
+++ 05_variables.py
@@ -0,0 +1,36 @@
+name = "Alex"
+print(name)
+
+# name = 35
+# print(name)
+#
+# name = False
+# print(name)
+
+age = 35
+print(age+1.58)
+print(age - 1)
+
+print("Hello, " + name)
+print(name * 5)
+# age = "Alexander"
+# print(age + 1)
+
+# price = 45.99
+#
+# is_admin = False
+
+# my_country_1948 = "Israel"
+# my_Country_1948 = "Israel1948"
+# print(my_Country_1948)
+# print(my_country_1948)
+
+# base_url = "https://www.google.co.il"
+# print(base_url)
+# print(base_url)
+# print(base_url)
+# print(base_url)
+# print(base_url)
+# print(base_url)
+# print(base_url)
+
