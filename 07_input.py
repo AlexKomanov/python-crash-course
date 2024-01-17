542b0bfec4f74889a92cbed0ce6fabab1a15124b
AlexKomanov <akomanov88@gmail.com>
2024-01-17 16:57:55 +0200

    Adding 07_input.py

---
+++ 07_input.py
@@ -0,0 +1,22 @@
+# name = input("Please enter your name: ")
+#
+# print(f"Hello, {name}")
+#
+number_1 = int(input("Please enter number 1: "))
+number_2 = int(input("Please enter number 2: "))
+
+print(type(number_2))
+print(type(number_1))
+
+result = number_1 + number_2
+print(f"Result: {result}")
+
+print(type(result))
+
+result_string = str(result)
+print(type(result_string))
+
+# print(int("ABC"))  # ValueError: invalid literal for int() with base 10: 'ABC'
+
+
+
