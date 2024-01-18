767dfe6d48cd8cbdcfad5a1f060fdc42ccda5b58
AlexKomanov <akomanov88@gmail.com>
2024-01-18 16:11:41 +0200

    Adding 09_if_elif_else_statements.py

---
+++ 09_if_elif_else_statements.py
@@ -0,0 +1,13 @@
+age = int(input("What is your age: "))
+
+if age >= 67:
+    print("You're above 67")
+
+if age >= 18:
+    print("You're adult!")
+elif age == 18:
+    print("You're 18 years old!")
+else:
+    print("You're too young...")
+
+print("The app run was finished")
