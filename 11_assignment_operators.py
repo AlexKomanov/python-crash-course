128988a005826858709eb2d64f0586b80dcace2d
AlexKomanov <akomanov88@gmail.com>
2024-01-18 19:45:08 +0200

    Adding 11_assignment_operators.py

---
+++ 11_assignment_operators.py
@@ -0,0 +1,30 @@
+
+# number = 5
+# print(number)
+# number += 5  # number = number + 5
+# print(number)
+
+# number = 10
+# print(number)
+# number -= 5  # number = number - 5
+# print(number)
+
+# number = 10
+# print(number)
+# number *= 5  # number = number * 5
+# print(number)
+
+# number = 3
+# print(number)
+# number **= 4  # number = number ** 4
+# print(number)
+
+number = 103
+number //= 4  # number = number // 4
+print(number)
+number /= 4  # number = number / 4
+print(number)
+number %= 4  # number = number % 4
+print(number)
+
+
