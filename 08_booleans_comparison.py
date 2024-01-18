a5a2e2d804b45310edbbc6026af72682323f0a22
AlexKomanov <akomanov88@gmail.com>
2024-01-18 15:44:54 +0200

    Adding 08_booleans_comparison.py

---
+++ 08_booleans_comparison.py
@@ -0,0 +1,16 @@
+# booleans True / False
+# is_true = False
+# print(is_true)
+# print(type(is_true))
+
+# Comparison operators: > / < / <= / >= / == / !=
+# print(6 == 6)
+
+# print("Alex" == "alex")
+
+print(ord("A"))
+print(ord("a"))
+print(ord(" "))
+
+#     65  == 97
+print("A" == 'a')
