67f12ed0ad25ff9df57f68aef38ffa6b71e8d0da
AlexKomanov <akomanov88@gmail.com>
2024-01-19 16:08:45 +0200

    Adding 15_in_operator.py

---
+++ 15_in_operator.py
@@ -0,0 +1,16 @@
+value = "Hello World Hello Alex Hello All"
+# value = ["Hello", "World!", "Alex"]
+# value = ("Hello", "World!", "Alex")
+
+# if "Hello" in value:
+#     print("Value exists")
+# else:
+#     print("Value doesn't exist")
+
+if value.count("Hello") > 0:
+    print("Value exists")
+else:
+    print("Value doesn't exist")
+
+print(value.count("Hello"))
+
