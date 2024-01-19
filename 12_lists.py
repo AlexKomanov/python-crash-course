203b6f4117d88b214cd642eea666d7671b505b17
AlexKomanov <akomanov88@gmail.com>
2024-01-19 15:14:28 +0200

    Adding 12_lists.py

---
+++ 12_lists.py
@@ -0,0 +1,28 @@
+#                               -3   -2   -1
+#          0    1        2      3     4    5
+my_list = [1, True, [3, 4, 10], 5, "Alex", 7, "Alex"]
+print(my_list.index(5))
+# print(my_list)
+# print(my_list[1])
+# print(my_list[0:4])
+# print(my_list[0:5])
+# print(my_list[0:5:2])
+# print(my_list[0:5:2])
+# print(my_list[-4:-1])
+# print(my_list[-4:])
+# print(my_list[::-1])
+# print(my_list[2][2])
+
+my_list = [10, 5, 4, 1, 1000]
+print(my_list)
+# print(my_list.pop(2))
+# print(my_list.count("Alex))
+# my_list.append("Komanov")
+# my_list += 53, 53
+# my_list.reverse()
+my_list.sort()
+print(my_list)
+my_list.sort(reverse=True)
+print(my_list)
+
+
