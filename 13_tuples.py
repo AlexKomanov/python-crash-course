1327ce831f9785689ef61679285a90fb237aa4ce
AlexKomanov <akomanov88@gmail.com>
2024-01-19 15:26:26 +0200

    Adding 13_tuples.py

---
+++ 13_tuples.py
@@ -0,0 +1,27 @@
+my_list = [1, 2, 3, 4, 5]
+print(my_list)
+print(id(my_list))
+my_list[2] = 10
+print(my_list)
+print(id(my_list))
+
+my_tuple = (1, 2, 3, 4, 3)
+# print(my_tuple)
+# my_tuple[2] = 10
+# print(my_tuple)
+# print(my_tuple.count(3))
+# print(type(my_tuple))
+# print(my_tuple)
+#
+# print(my_tuple[2])
+
+# print(my_tuple)
+# print(id(my_tuple))
+#
+# list_from_tuple = list(my_tuple)
+# print(list_from_tuple)
+# list_from_tuple[-1] = 5
+# print(list_from_tuple)
+# my_tuple = tuple(list_from_tuple)
+# print(my_tuple)
+# print(id(my_tuple))
