83f2c06b2dd8fc2122b728eee80c9ff00db7fac2
AlexKomanov <akomanov88@gmail.com>
2024-01-26 12:29:10 +0200

    Adding 27_python_library.py

---
+++ 27_python_library.py
@@ -0,0 +1,12 @@
+# https://docs.python.org/3/library/index.html
+import math
+from string import digits, ascii_letters
+import datetime
+
+
+print(math.sqrt(16))
+print(digits)
+print(ascii_letters)
+print(type(digits))
+print(datetime.date.today())
+
